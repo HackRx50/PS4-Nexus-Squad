efbff74c3586f24934fc519a9b6320337a2874f7
abhi6774 <abhishekmourya6774@gmail.com>
2024-09-16 11:32:25 +0530

    repository cleanup

--- server_old/actions/cm0wq5qly0000u71rcq9t2h0p.py
+++
@@ -1,9 +0,0 @@
-"def hello_django():
-    """
-    Return a greeting for Django framework.
-
-    Returns:
-        str: A string greeting Django.
-    """
-    return "Hello Django"
-"
--- server_old/actions/cm0wq65rf0001u71root6bovd.py
+++
@@ -1,9 +0,0 @@
-"def hello_django2():
-    """
-    Return a greeting for Django framework.
-
-    Returns:
-        str: A string greeting Django.
-    """
-    return "Hello Django"
-"
